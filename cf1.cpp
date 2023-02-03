1d55f8549efd540ee8fd34dd993ec9f0a94dbcf2
101Sania <119449570+101Sania@users.noreply.github.com>
2023-02-03 21:42:33 +0600

    Add files via upload

---
+++ cf1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    char a;
+    while(t--){
+       cin>>a;
+       if(a=='c'||a=='o'||a=='d'||a=='e'||a=='f'||a=='r'||a=='s')
+       {
+           cout<<"yes"<<endl;
+       }
+       else{
+        cout<<"no"<<endl;
+       }
+    }
+}
