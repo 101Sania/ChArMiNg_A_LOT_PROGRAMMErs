57d00b081bfff3cdb7382b833a1ce507af52aaf5
101Sania <119449570+101Sania@users.noreply.github.com>
2023-06-03 17:15:58 +0600

    Update Problem.py

--- Problem.py
+++ Problem.py
@@ -1,2 +1,18 @@
 # python problems for beginners 
 
+#weigth kgs or Lbs:
+
+w = float(input("Weigth: "))
+
+wi = input("(K)g or (L)bs: ")
+
+if wi.upper() == "L":
+
+    print("Weigth in Kgs: ",w*0.45)
+
+else:
+
+    print("Weigth in Lbs: ",w/0.45)
+    
+    
+#    
